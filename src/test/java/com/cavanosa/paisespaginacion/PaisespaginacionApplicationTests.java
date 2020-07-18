42cca071ea219128215a815e29588d0001886285
jorgechavezrnd <jorgechavezrnd@gmail.com>
2020-07-18 18:22:17 -0400

    Paginacion funcionando

---
+++ src/test/java/com/cavanosa/paisespaginacion/PaisespaginacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cavanosa.paisespaginacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaisespaginacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
